f7a774c389d8bfc3b900f4047022546405702913
Ami <agianchandani2018@pingry.org>
2016-03-09 07:37:51 -0500

    Not working. First draft function only

---
+++ powerFunction.py
@@ -0,0 +1,22 @@
+#Author: Ami
+#Date: March 10
+
+def to_power():
+	base = int(input("Enter a base: "))
+	superScript = int(input("Enter an exponent: ")
+	
+	variable = "1"
+		while variable = "1":
+		
+			try:
+				number = float(number)
+			except ValueError:
+				print("Sorry, that's not a number.")
+				print("Try again")	
+
+
+
+
+# --------------------------------- Main program below -----------------------------------	
+
+
