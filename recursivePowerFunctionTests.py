1ce240bd8c7a34dc7874b120a97a0c985bd879dd
Ami <agianchandani2018@pingry.org>
2016-04-12 12:15:05 -0400

    Recursive power function tests. All working. Initial commit

---
+++ recursivePowerFunctionTests.py
@@ -0,0 +1,11 @@
+import to_power(base, exponent)	
+import int_input(question)
+import float_input(question)	
+
+
+print("{} to the power of {} is {}".format(0, 0, to_power(0, 0)))
+print("{} to the power of {} is {}".format(1, 2, to_power(1, 2)))
+print("{} to the power of {} is {}".format(3, 1, to_power(3, 1)))
+print("{} to the power of {} is {}".format(4, 0, to_power(4, 0)))
+print("{} to the power of {} is {}".format(-5, 2, to_power(-5, 2)))
+print("{} to the power of {} is {}".format(-5, -3, to_power(-5, -3)))
